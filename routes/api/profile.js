09411a167c8c756290bbc6afd1dee06abe24e1ae
Jason Lee <lcycug@live.com>
2019-02-27 14:27:04 +0800

    profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -27,15 +27,75 @@ router.get(
   passport.authenticate("jwt", { session: false }),
   (req, res) => {
     const errors = {};
-    Profile.findOne({ user: req.user.id }).then(profile => {
+    Profile.findOne({ user: req.user.id })
+      .populate("user", ["name", "avatar"])
+      .then(profile => {
+        if (!profile) {
+          errors.noprofile = "There is no profile for the current user.";
+          return res.status(404).json(errors);
+        }
+        res.json(profile);
+      });
+  }
+);
+
+/**
+ * @router  GET /api/profile/handle/:handle
+ * @desc    Get profile under a handle
+ * @access  Public
+ */
+router.get("/handle/:handle", (req, res) => {
+  const errors = {};
+  Profile.findOne({ handle: req.params.handle })
+    .populate("user", ["name", "avatar"])
+    .then(profile => {
       if (!profile) {
-        errors.noprofile = "There is no profile for the current user.";
-        return res.status(404).json(errors);
+        errors.noprofile = "There is no profile for this user.";
+        res.status(404).json(errors);
       }
       res.json(profile);
     });
-  }
-);
+});
+
+/**
+ * @router  GET /api/profile/user/:user_id
+ * @desc    Get profile under id of a user
+ * @access  Public
+ */
+router.get("/user/:user_id", (req, res) => {
+  const errors = {};
+  Profile.findOne({ user: req.params.user_id })
+    .populate("user", ["name", "avatar"])
+    .then(profile => {
+      if (!profile) {
+        errors.noprofile = "There is no profile for this user.";
+        res.status(404).json(errors);
+      }
+      res.json(profile);
+    })
+    .catch(err => {
+      errors.noprofile = "There is no profile for this user.";
+      res.json(errors);
+    });
+});
+
+/**
+ * @router  GET /api/profile/all
+ * @desc    Get all profiles
+ * @access  Public
+ */
+router.get("/all", (req, res) => {
+  const errors = {};
+  Profile.find()
+    .populate("user", ["name", "avatar"])
+    .then(profiles => {
+      if (!profiles) {
+        errors.noprofile = "There are no profiles in database.";
+        res.status(404).json(errors);
+      }
+      res.json(profiles);
+    });
+});
 
 /**
  * @router  POST /api/profile
