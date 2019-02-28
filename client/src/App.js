af96030d3187fe83b1c3ebbbb4a6d8eee6e243dc
Jason Lee <lcycug@live.com>
2019-02-28 12:49:50 +0800

    routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,23 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 import "./App.css";
-import Navbar from "./components/Layout/Navbar";
-import Landing from "./components/Layout/Landing";
-import Footer from "./components/Layout/Footer";
+import Navbar from "./components/layout/Navbar";
+import Landing from "./components/layout/Landing";
+import Footer from "./components/layout/Footer";
+import Login from "./components/auth/Login";
+import Register from "./components/auth/Register";
 
 class App extends Component {
   render() {
     return (
       <>
         <Navbar />
-        <Landing />
+        <Switch>
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/register" component={Register} />
+        </Switch>
         <Footer />
       </>
     );
