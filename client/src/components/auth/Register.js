c358a54fad062a89c9dfb378458d788e72815359
Jason Lee <lcycug@live.com>
2019-02-28 13:29:38 +0800

    Login and registration page

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,7 +1,90 @@
 import React, { Component } from "react";
 
 export default class Register extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      email: "",
+      password: "",
+      password2: ""
+    };
+  }
+
+  handleChange = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    console.log(this.state);
+  };
+
   render() {
-    return <div>Register</div>;
+    return (
+      <div className="register">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-8 m-auto">
+              <h1 className="display-4 text-center">Sign Up</h1>
+              <p className="lead text-center">
+                Create your DevConnector account
+              </p>
+              <form onSubmit={e => this.handleSubmit(e)}>
+                <div className="form-group">
+                  <input
+                    type="text"
+                    className="form-control form-control-lg"
+                    placeholder="Name"
+                    autoComplete="new-password" //'off' not work for Chrome
+                    name="name"
+                    value={this.state.name}
+                    onChange={e => this.handleChange(e)}
+                  />
+                </div>
+                <div className="form-group">
+                  <input
+                    type="email"
+                    className="form-control form-control-lg"
+                    placeholder="Email Address"
+                    autoComplete="off"
+                    name="email"
+                    value={this.state.email}
+                    onChange={e => this.handleChange(e)}
+                  />
+                  <small className="form-text text-muted">
+                    This site uses Gravatar so if you want a profile image, use
+                    a Gravatar email
+                  </small>
+                </div>
+                <div className="form-group">
+                  <input
+                    type="password"
+                    className="form-control form-control-lg"
+                    placeholder="Password"
+                    autoComplete="off"
+                    name="password"
+                    value={this.state.password}
+                    onChange={e => this.handleChange(e)}
+                  />
+                </div>
+                <div className="form-group">
+                  <input
+                    type="password"
+                    className="form-control form-control-lg"
+                    placeholder="Confirm Password"
+                    autoComplete="off"
+                    name="password2"
+                    value={this.state.password2}
+                    onChange={e => this.handleChange(e)}
+                  />
+                </div>
+                <input type="submit" className="btn btn-info btn-block mt-4" />
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
