854d08b513354ec374835d21ad01b01f753054ee
Jason Lee <lcycug@live.com>
2019-02-28 09:19:56 +0800

    Landing page

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,18 @@
 import React, { Component } from "react";
 import "./App.css";
+import Navbar from "./components/Layout/Navbar";
+import Landing from "./components/Layout/Landing";
+import Footer from "./components/Layout/Footer";
 
 class App extends Component {
   render() {
-    return <div>Hello World!</div>;
+    return (
+      <>
+        <Navbar />
+        <Landing />
+        <Footer />
+      </>
+    );
   }
 }
 
